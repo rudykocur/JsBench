0a3f4daaedc80f20b52752bcd9d86f1299873129
rudykocur <grzegorz.przydryga@gmail.com>
2012-12-19 22:03:35 +0100

    Page title ;)

--- application/views/header.php
+++ application/views/header.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome to CodeIgniter</title>
+	<title>OESK - Js Benchmark Suite</title>
         <script src="<?php echo base_url() ?>assets/moo.js"></script>
         <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>assets/styles.css">
 
